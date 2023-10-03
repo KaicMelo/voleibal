39cb1cb6b5c693f0a3a026a3d1f3c04658a844a8
Kaic <kaic.melo@totvs.com.br>
2023-10-03 01:35:48 -0300

    fix: limit in player and check if player keep in other team

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -6,8 +6,14 @@ import {
   Output,
   OnInit,
 } from '@angular/core';
-import { PoModalComponent } from '@po-ui/ng-components';
+import {
+  PoModalComponent,
+  PoNotification,
+  PoNotificationService,
+  PoToasterOrientation,
+} from '@po-ui/ng-components';
 import { TeamService } from 'src/app/services/team/team.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-modal',
@@ -25,12 +31,23 @@ export class ModalComponent implements OnInit {
 
   @Output() onClose = new EventEmitter<boolean>();
 
-  constructor(private teamService: TeamService) {}
+  constructor(
+    private teamService: TeamService,
+    private userService: UserService,
+    private poNotificationService: PoNotificationService
+  ) {}
 
   async ngOnInit(): Promise<void> {
     this.modal.open();
     this.teamService.get().subscribe((value) => {
       this.team = value;
+    });
+
+    const team: any = [];
+    this.content.filter((fn: any) => {
+      if (fn != event) {
+        team.push(fn);
+      }
     });
   }
 
@@ -40,13 +57,48 @@ export class ModalComponent implements OnInit {
 
   async add(event: any) {
     if (this.team) {
+      if (this.team.team_2.includes(event)) {
+        const poNotification: PoNotification = {
+          message: `${event.name} está jogando no time adversário`,
+          orientation: PoToasterOrientation.Top,
+        };
+        this.poNotificationService.information(poNotification);
+        return;
+      }
+
       if (this.teamNumber == 1) {
         if (!this.team.team_1.includes(event)) {
+          if (this.team.team_1.length == 6) {
+            const poNotification: PoNotification = {
+              message: '6 é limite máximo de jogadores',
+              orientation: PoToasterOrientation.Top,
+            };
+            this.poNotificationService.information(poNotification);
+            return;
+          }
           this.team.team_1.push(event);
         }
         this.teamService.put(this.team.team_1, this.team.team_2);
       } else {
+        if (this.team.team_1.includes(event)) {
+          const poNotification: PoNotification = {
+            message: `${event.name} está jogando no time adversário`,
+            orientation: PoToasterOrientation.Top,
+          };
+          this.poNotificationService.information(poNotification);
+          return;
+        }
+
         if (!this.team.team_2.includes(event)) {
+          if (this.team.team_2.length == 6) {
+            const poNotification: PoNotification = {
+              message: '6 é limite máximo de jogadores',
+              orientation: PoToasterOrientation.Top,
+            };
+            this.poNotificationService.information(poNotification);
+            return;
+          }
+
           this.team.team_2.push(event);
         }
         this.teamService.put(this.team.team_1, this.team.team_2);
--- src/app/shared/modal/modal.module.ts
+++ src/app/shared/modal/modal.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
-import { PoModalModule, PoModule } from '@po-ui/ng-components';
+import { PoModalModule, PoModule, PoNotificationModule } from '@po-ui/ng-components';
 
 @NgModule({
   declarations: [ModalComponent],
   exports: [ModalComponent],
-  imports: [CommonModule, PoModule, PoModalModule],
+  imports: [CommonModule, PoModule, PoModalModule,PoNotificationModule],
 })
 export class ModalModule {}
